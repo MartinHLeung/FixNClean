bb90539f4cafd2a0a3549f17ee2c368470dbf52f
Dlynch16 <36079570+Dlynch16@users.noreply.github.com>
2018-03-14 11:28:47 -0400

    Updated to work with 3.6 instead of 2.7
    we switched to 3.6 cuz 2.7 was too old for maps api to work with it

--- FixNClean.py
+++ FixNClean.py
@@ -26,11 +26,15 @@ groupCounter = 0
 dates = ['Saturday September 30th - 9:00-12:00', 'Saturday September 30th - 1:00-4:00', 'Sunday October 1st - 9:00-12:00', 'Sunday October 1st - 1:00-4:00', "Only my first option works :("]
 for i in range(1,sheet.nrows):
     for j in range(5):
-        volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]] = [str(sheet.cell(i, 2+j*4))[7:-1], str(sheet.cell(i, 3+j*4))[7:-1], dates.index(str(sheet.cell(i, 20))[7:-1]), dates.index(str(sheet.cell(i, 21))[7:-1]), [l for l in str(sheet.cell(i, 22))[7:-1].split(",")]]
-        if sheet.cell(i, 4+j*4) == "No":
+        volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]] = [str(sheet.cell(i, 2+j*4))[6:-1], str(sheet.cell(i, 3+j*4))[6:-1], dates.index(str(sheet.cell(i, 20))[6:-1]), dates.index(str(sheet.cell(i, 21))[6:-1]), [l for l in str(sheet.cell(i, 22))[6:-1].split(",")]]
+        if str(sheet.cell(i, 4+j*4))[6:-1] == "No":
             break
         elif j>0:
             groups[str(groupCounter)].addmember(str(sheet.cell(i, 1+j*4))[7:-2], volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]][-1])
         else:
             groupCounter += 1
-            groups[str(groupCounter)] = group([str(sheet.cell(i, 1+j*4))[7:-2]], volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]][2:3], volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]][-1])
+            groups[str(groupCounter)] = group([str(sheet.cell(i, 1+j*4))[7:-2]], volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]][2:4], volunteerInformation[str(sheet.cell(i, 1+j*4))[7:-2]][-1])
+
+print(volunteerInformation)
+for i in groups.keys():
+    print(groups[i].members, groups[i].timeslot)
